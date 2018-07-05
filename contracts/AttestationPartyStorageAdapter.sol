038402a46232bf6b41360e22e963548fa46298ea
Bogdan Hrytsenko <bhrytsenko@edenlab.com.ua>
2018-07-05 16:13:28 +0300

    AttestationPartyStorageAdapter AccessToFieldVerification methods access restrictions added

--- contracts/AttestationPartyStorageAdapter.sol
+++ contracts/AttestationPartyStorageAdapter.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.23;
 
 import "./BaseStorage.sol";
 import "./WithKimlicContext.sol";
+import "./KimlicContractsContext.sol";
 import "./AttestationPartyStorage.sol";
 import "./BasePartyStorageAdapter.sol";
 
@@ -10,27 +11,34 @@ contract AttestationPartyStorageAdapter is BasePartyStorageAdapter {
 
     /// private attributes ///
     string private constant accessKeyPart = "IsVerificaionAllowed";
-    
+
     /// constructors ///
     constructor (address contextStorage) public BasePartyStorageAdapter(contextStorage) {
         
     }
 
-    function addAccessToFieldVerification(address party, string fieldName) public {//TODO access restrictions
-        require(getContext().getAccountStorageAdapter().isAllowedFieldName(fieldName));
+    function addAccessToFieldVerification(address party, string fieldName) public {
+        KimlicContractsContext context = getContext();
+        require(msg.sender == context.owner());
+        require(context.getAccountStorageAdapter().isAllowedFieldName(fieldName));
 
         AttestationPartyStorage storageAdapter = AttestationPartyStorage(getStorage());
         storageAdapter.setBool(getAllowanceKey(party, fieldName), true);
     }
 
     function getIsFieldVerificationAllowed(address party, string fieldName) public view returns(bool) {
-        require(getContext().getAccountStorageAdapter().isAllowedFieldName(fieldName));
+        KimlicContractsContext context = getContext();
+        require(context.getAccountStorageAdapter().isAllowedFieldName(fieldName));
 
         AttestationPartyStorage storageAdapter = AttestationPartyStorage(getStorage());
         return storageAdapter.getBool(getAllowanceKey(party, fieldName));
     }
 
-    function removeAccessToFieldVerification(address party, string fieldName) public {//TODO access restrictions
+    function removeAccessToFieldVerification(address party, string fieldName) public {
+        KimlicContractsContext context = getContext();
+        require(msg.sender == context.owner());
+        require(context.getAccountStorageAdapter().isAllowedFieldName(fieldName));
+
         AttestationPartyStorage storageAdapter = AttestationPartyStorage(getStorage());
         storageAdapter.deleteBool(getAllowanceKey(party, fieldName));
     }
